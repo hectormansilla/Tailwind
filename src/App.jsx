797f94ba742d58ba4f1cde7a8f3f0a2cebafca67
Hector Adrian Mansilla Arias <Hector.Adrian.Mansilla.Arias@kyndryl.com>
2023-04-27 14:55:21 -0400

    1. instalación y uso de GRID

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  
+  return (
+
+      <div className="container">
+        <div className="grid grid-flow-col grid-rows-3 grid-cols-4 gap-4">
+          <div className="bg-blue-100">1</div>
+          <div className="bg-blue-200">2</div>
+          <div className="bg-blue-300">3</div>
+          <div className="bg-blue-400">4</div>
+          <div className="bg-blue-500">5</div>
+          <div className="bg-blue-600">6</div>
+          <div className="bg-blue-700">7</div>
+          <div className="bg-blue-800">8</div>
+          <div className="bg-blue-900">9</div>
+        </div>  
+      </div>
+
+  )
+}
+
+export default App
