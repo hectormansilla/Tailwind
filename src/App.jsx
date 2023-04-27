7d6213e4f67bd736f8c9f7d94100aa9ac9c94b2c
Hector Adrian Mansilla Arias <Hector.Adrian.Mansilla.Arias@kyndryl.com>
2023-04-27 16:24:35 -0400

    3. Columns

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,20 @@ function App() {
   return (
 
     <div className="container mx-auto py-12">
-      <div className="grid grid-cols-3 gap-6">
-        <div className="col-span-2">
-          <iframe className="w-full aspect-video" src="https://www.youtube.com/embed/r1ySsbIA7EU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-          <div className="bg-red-400 aspect-square">
-
-          </div>
-        </div>
-        <div className="col-span-1 bg-blue-200">
-
-        </div>
+      <div className="columns-3xs gap-6">
+        <img src="https://cdn.pixabay.com/photo/2023/04/24/02/51/crimson-rosella-7947000_1280.jpg" alt="" />
+        <img src="https://cdn.pixabay.com/photo/2023/04/15/14/21/bird-7927829_1280.jpg" alt="" />
+        <img src="https://cdn.pixabay.com/photo/2023/03/30/11/04/plants-7887485_1280.jpg" alt="" />
+        <img src="https://cdn.pixabay.com/photo/2023/04/22/22/58/bird-7944578_1280.jpg" alt="" />
+        <img src="https://cdn.pixabay.com/photo/2023/04/18/08/42/children-7934514_1280.jpg" alt="" />
+        <img src="https://cdn.pixabay.com/photo/2023/04/19/10/54/horse-7937478_1280.jpg" alt="" />
+      </div>
+      <div className="columns-2 gap-6">
+        <p className="bg-red-200">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ultricies lectus a leo semper, quis finibus sem congue. Etiam dapibus tempus velit a tempor. Pellentesque accumsan sapien sed lobortis rutrum. Nam nec dui magna. Nulla cursus convallis nibh, id venenatis neque elementum a. Pellentesque eget enim non urna dignissim pretium sed a lectus. Vestibulum arcu augue, congue eget sapien id, dapibus finibus metus. Curabitur efficitur condimentum erat in molestie. Sed eleifend pretium odio, eget viverra tellus tempus vel. Aenean non aliquet erat. Nam dignissim arcu vitae sollicitudin bibendum. Donec et velit at purus aliquam pharetra et vel lectus.</p>
+        <p className="bg-blue-200">Aliquam ac pharetra velit. Cras a pretium tortor, et posuere risus. Donec non felis vitae orci ultricies tincidunt quis non dolor. Proin turpis dolor, ornare sit amet arcu ut, rhoncus pharetra dui. Donec nec magna sed nunc feugiat ultricies. Mauris tristique volutpat diam sed ultrices. Pellentesque varius dictum vestibulum. Sed ac neque id magna bibendum fermentum congue vel leo. Proin finibus lacus at ligula semper pretium. In cursus arcu ex, ac rhoncus eros efficitur eu. Ut dapibus dictum iaculis. Duis dapibus libero at orci commodo cursus. Nunc eu imperdiet nulla, vitae rhoncus purus. Vestibulum tempus dapibus neque sit amet mattis. Vestibulum quis turpis sapien.</p>
+        <p className="bg-green-200 break-inside-avoid-column">Nunc suscipit tortor sit amet mattis rhoncus. Pellentesque sed euismod turpis. Mauris laoreet arcu neque, ut posuere felis ullamcorper congue. Praesent risus lacus, semper ac libero a, semper dictum nisl. Morbi scelerisque massa a ipsum ultrices fringilla. Quisque nec urna non nulla ultrices luctus sit amet nec mi. Aenean rhoncus ligula non dolor eleifend placerat. Vestibulum vel purus et massa bibendum condimentum. Maecenas id sem enim. Suspendisse ac nisl augue. Curabitur egestas ligula quis dolor facilisis ultricies. Sed pellentesque vulputate mi a facilisis. Maecenas pretium lorem quis mi pulvinar, sed mattis urna tempor.</p>
+        <p className="bg-indigo-200">Aliquam a ligula at velit scelerisque pretium. In id ipsum pellentesque, imperdiet est in, maximus tortor. Vivamus vestibulum enim bibendum enim vulputate lobortis. Cras vehicula tincidunt sem eget feugiat. Phasellus sodales ut nibh at commodo. Nulla facilisi. Morbi tristique venenatis dolor ac cursus.</p>
+        <p className="bg-yellow-200">Cras et ullamcorper massa. Donec malesuada elit vitae diam finibus rhoncus. Nullam sollicitudin dolor non nisl placerat, at accumsan nisi fringilla. Nam vitae feugiat purus, nec convallis augue. Praesent condimentum sodales faucibus. Fusce tempus, mi sit amet sodales rutrum, lacus nisi lacinia sem, vel tincidunt enim dolor eu ante. Aliquam eu mi rutrum, aliquam tellus non, malesuada mi. Phasellus convallis libero id nisl egestas ornare. Curabitur urna ipsum, condimentum sed tellus id, accumsan fringilla elit. Curabitur nec nibh at risus placerat accumsan. Pellentesque ipsum magna, bibendum at rutrum id, condimentum sit amet velit. Phasellus in consectetur tellus. Proin ac elit fringilla, ultricies erat in, finibus urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
       </div>
     </div>
 
